7f88ac17fd196ac1045a10855c6a0583a11c40f5
Benjamin Stephanus <ben.stephanus@gmail.com>
2020-07-22 17:59:34 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,15 @@
 class Dog 
   def name=(name)
-    @this_dogs_name = name 
+    @name = name 
   end 
   
   def name 
     @this_dogs_name 
   end 
   
+  def breed=(breed)
+    @this_dogs_breed
+  end 
   
+  def breed 
+    
